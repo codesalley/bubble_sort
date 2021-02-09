2ea0a2f7a4e0c598eeca3e3d6c20e94406c6084e
juxsalley <juxsalley@gmail.com>
2021-02-09 13:47:25 +0000

    add bubble_sort_by method

---
+++ bubble_sort_by.rb
@@ -0,0 +1,12 @@
+def bubble_sort_by(arr)
+  arr.length.times do
+    arr.each_with_index do |_x, i|
+      left = arr[i]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[-1] != (left) && yield(arr[i], arr[i + 1]).positive?
+    end
+  end
+  arr
+end
+p bubble_sort_by(%w[hi hello hey]) { |left, right|
+  left.length - right.length
+}
